065b2784385dfd9f5003087a174738176e9d8d27
Nikita Elohin <nikita.elohin@itlab-pro.ru>
2024-06-19 11:06:53 +0300

    added some chenges to appointment entity

--- src/main/java/beauty/models/Appointment.java
+++ src/main/java/beauty/models/Appointment.java
@@ -19,6 +19,8 @@ public class Appointment extends BaseEntity {
 	@Column(name = "time")
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	LocalDateTime time;
+
+	String description;
 	
 	@ManyToOne
 	@JoinColumn(name = "saloon_id")
